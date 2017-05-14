01cdf5d41bac252c4537fa40bb3ce37ea46a7dbf
creestian <creestian@10.0.0.2>
2017-05-14 00:17:46 -0300

    Classes created and added
    NPC Constructor added

---
+++ slayerGame/src/slayergame/model/NPC.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package slayergame.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+/**
+ *
+ * @author creestian
+ */
+public class NPC implements Serializable{
+    
+    private String name;
+
+    public NPC() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 67 * hash + Objects.hashCode(this.name);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final NPC other = (NPC) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "NPC{" + "name=" + name + '}';
+    }
+    
+
+    
+}
