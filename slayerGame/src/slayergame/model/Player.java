2176f46f1f08c6c8b5a586738f0a7472cc16739e
Rodriguez-Marcelo <neroig96@gmail.com>
2017-05-13 23:50:28 -0300

    02/25/17 - Slayers project created. Contro, Model, and View packages have been created and tested.

---
+++ slayerGame/src/slayergame/model/Player.java
@@ -0,0 +1,100 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package slayergame.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ *
+ * @author Chicho
+ */
+public class Player implements Serializable{
+    
+    private String name;
+    private int health;
+    private int mana;
+    private int strength;
+
+    public Player() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getMana() {
+        return mana;
+    }
+
+    public void setMana(int mana) {
+        this.mana = mana;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 29 * hash + Objects.hashCode(this.name);
+        hash = 29 * hash + this.health;
+        hash = 29 * hash + this.mana;
+        hash = 29 * hash + this.strength;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Player other = (Player) obj;
+        if (this.health != other.health) {
+            return false;
+        }
+        if (this.mana != other.mana) {
+            return false;
+        }
+        if (this.strength != other.strength) {
+            return false;
+        }
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" + "name=" + name + ", health=" + health + ", mana=" + mana + ", strength=" + strength + '}';
+    }
+    
+    
+}
