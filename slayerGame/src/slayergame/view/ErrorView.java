b87426d9082cca0f7f77c7131e338ec055ad06fa
creestian <creestian@10.0.0.2>
2017-07-10 23:42:30 -0300

    ErrorView class PrintRWriter errorFile

---
+++ slayerGame/src/slayergame/view/ErrorView.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package slayergame.view;
+
+import java.io.PrintWriter;
+
+/**
+ *
+ * @author creestian
+ */
+public class ErrorView {
+    
+    private static final PrintWriter errorFile = getOutFile();
+    
+    public static void display(String className, String errorMessage) {
+        
+        errorFile.println(
+        "+----------------------------------------------+"
+        + "\n- ERROR - " + errorMessage
+        + "\n +----------------------------------------------+"
+        );
+    }
+    
+}
